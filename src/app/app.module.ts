a97ed6416a43b0dce71de961bb0265c51eafdfc5
jerdos-s <jeremy.dosg@gmail.com>
2023-08-07 21:22:21 +0200

    add data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,22 +8,34 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
+import { HttpClientModule } from "@angular/common/http";
+import { ShippingComponent } from './shipping/shipping.component';
 
 const rootPath = [
-  { path: '', component: ProductListComponent },
-  { path: 'products/:productId', component: ProductDetailsComponent },
+    { path: '', component: ProductListComponent },
+    { path: 'products/:productId', component: ProductDetailsComponent },
+    { path: 'cart', component: CartComponent },
+    { path: 'shipping', component: ShippingComponent },
 ];
 
 @NgModule({
-  imports: [BrowserModule, ReactiveFormsModule, RouterModule.forRoot(rootPath)],
-  declarations: [
-    AppComponent,
-    TopBarComponent,
-    ProductListComponent,
-    ProductAlertsComponent,
-    ProductDetailsComponent,
-  ],
-  bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+        RouterModule.forRoot(rootPath),
+        HttpClientModule
+    ],
+    declarations: [
+        AppComponent,
+        TopBarComponent,
+        ProductListComponent,
+        ProductAlertsComponent,
+        ProductDetailsComponent,
+        CartComponent,
+        ShippingComponent,
+    ],
+    bootstrap: [AppComponent],
 })
 export class AppModule {}
 
--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,17 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product, products } from '../products';
+import {CartService} from "../cart.service";
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css'],
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit{
   product: Product | undefined;
 
-  constructor(private route: ActivatedRoute) {}
-
+  constructor(
+      private route: ActivatedRoute,
+      private cartService: CartService
+  ) {}
   ngOnInit() {
     const routeParams = this.route.snapshot.paramMap;
     const productIdFromRoute = Number(routeParams.get('productId'));
@@ -20,4 +23,10 @@ export class ProductDetailsComponent {
       (product) => product.id === productIdFromRoute
     );
   }
+
+  addToCart(product: Product) :void{
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart!');
+  }
+
 }
